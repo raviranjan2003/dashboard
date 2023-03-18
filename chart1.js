3b3dfc83f25f90946520c6cb92956d3683ab4a0b
Ravi <2140131@sliet.ac.in>
2023-03-18 16:11:18 +0530

    dashboard for kalaverse

---
+++ chart1.js
@@ -0,0 +1,21 @@
+var ctx = document.getElementById('lineChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+        datasets: [{
+            label: 'Points',
+            data: [2050, 1900, 2100, 2800, 1800, 2000, 2500, 2600, 2450, 1950, 2300, 2900],
+            backgroundColor: [
+                'rgba(85,85,85, 1)'
+
+            ],
+            borderColor: 'rgb(189, 137, 245)',
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
